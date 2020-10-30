a466434303d98574f443a1f0fe799cc7a7431a11
thatboisuss <72503254+thatboisuss@users.noreply.github.com>
2020-10-30 20:18:21 +0800

    Update D.js

--- D.js
+++ D.js
@@ -1,14 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
-
-client.login('NzM2NjMxNjM3NjMwMjU1MTY1.XxxnrA.tJUqS3xQolsZw8My7y0GBgGIoHI');
+console.log("ur gay u looked at this file smh")
